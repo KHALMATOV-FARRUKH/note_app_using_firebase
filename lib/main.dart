8fff621bf96bdcbda659e410397f3a9ee442567f
Fara <115084566+KHALMATOV-FARRUKH@users.noreply.github.com>
2022-10-19 18:22:07 +0500

    The following packages have been added: flutter pub add firebase_core flutter pub add cloud_firestore flutter pub add google_fonts
    Created by:
    app_style.dart
    note_card.dart
    note_editor.dart
    note_reader.dart
    files dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'screens/home_screen.dart';
 
-void main() {
+Future<void> main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -11,7 +14,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: HomeScreen(),
     );
   }
